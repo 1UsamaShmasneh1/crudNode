94e35aaae0ff237ee96ae0b60aa294f34cb3c1c1
usama shmasneh <us.05.07.91@gmail.com>
2022-04-30 20:26:14 +0300

    check

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient
 
 const app = express();
+
+MongoClient.connect('mongodb://localhost:27017', (err, db) => {
+    if(err){
+        console.log('Can not connect to mongodb')
+        process.exit(1)
+    }
+    console.log('Connected to mongodb')
+})
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '\\index.html');
 });
+
+app.post('/quotes', (req,res) => {
+    console.log(req.body)
+})
 
 app.listen(3000, () => {
     console.log('Server is listening');
